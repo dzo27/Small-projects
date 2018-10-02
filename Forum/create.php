b492124c8cabff4b10a08d2185beb6e67c025981
dzo27 <m.kuzminska27@gmail.com>
2018-10-02 13:54:40 +0200

    Added static Inner Page Templates

---
+++ Forum/create.php
@@ -0,0 +1,13 @@
+<?php
+
+require("core/init.php");
+
+//Get Templates and Assign Vars
+$template = new Template("templates/create.php");
+
+//Assign Vars
+
+
+//Display template
+echo $template;
+?>
---
+++ Forum/topic.php
@@ -0,0 +1,13 @@
+<?php
+
+require("core/init.php");
+
+//Get Templates and Assign Vars
+$template = new Template("templates/topic.php");
+
+//Assign Vars
+
+
+//Display template
+echo $template;
+?>
